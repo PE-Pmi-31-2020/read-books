df3f341b1c571df4d024d6b8d758c6c3af1d3ca4
S1NED <denic.poruchnik@gmail.com>
2020-10-23 17:49:29 +0300

    Console application created. Postgres db connected

---
+++ read-books-app/read-books-app/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Npgsql;
+
+namespace read_books_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=read-books-db";
+
+            using var connection = new NpgsqlConnection(connectionString);
+
+            connection.Open();
+
+            //var sql = "SELECT * FROM users";
+
+            //using var cmd = new NpgsqlCommand(sql, connection);
+
+            //using NpgsqlDataReader rdr = cmd.ExecuteReader();
+
+            //while (rdr.Read())
+            //{
+            //    Console.WriteLine("{0} {1} {2}", rdr.GetInt32(0), rdr.GetString(1),
+            //            rdr.GetString(2));
+            //}
+        }
+    }
+}
