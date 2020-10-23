4ccd9088775b3b50cfe7918a7ad1e007c5ed896a
Erik <erik.mirai@outlook.com>
2020-10-23 21:37:22 +0300

    methods for generating data added

--- read-books-app/read-books-app/Program.cs
+++ read-books-app/read-books-app/Program.cs
@@ -1,17 +1,73 @@
 ﻿using System;
+using System.Linq;
 using Npgsql;
 
 namespace read_books_app
 {
     class Program
     {
+        static string EmailGenerator()
+        {
+            string[] names = { "bob", "alice", "alex", "tom", "emma", "ada", "olivia", "aiden", "finn", "anna" };
+            string[] mails = { "gmail.com", "ukr.net", "outlook.com" };
+            Random random = new Random();
+            return $"{names[random.Next(names.Length)]}{random.Next(100)}@{mails[random.Next(mails.Length)]}";
+        }
+        static string AuthorGenerator()
+        {
+            Random random = new Random();
+            string[] firstNames = { "Bob", "Alice", "Alex", "Tom", "Emma", "Ada", "Olivia", "Aiden", "Finn", "Anna" };
+            string[] lastNames = { "White", "Yellow", "Orange", "Red", "Blue", "Pink", "Purple", "Brown", "Grey", "Black" };
+            return $"{firstNames[random.Next(firstNames.Length)]} {lastNames[random.Next(lastNames.Length)]}";
+        }
+        static string PasswordGenerator()
+        {
+            Random random = new Random();
+            const string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, random.Next(8, 20))
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        static int PagesGenerator()
+        {
+            Random random = new Random();
+            return random.Next(5, 1000);
+        }
+        static void InsertData(string connectionString, NpgsqlConnection npgsql)
+        {
+            string reviewTemplate = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            Random random = new Random();
+            int amountOfUsers = 40;
+            int amountOfBooks = 35;
+            int amountOfStatistic = 47;
+            npgsql.Open();
+            using var cmd = new NpgsqlCommand();
+            cmd.Connection = npgsql;
+            for (int i = 0; i < amountOfUsers; i++)
+            {
+                cmd.CommandText = $"INSERT INTO users(email, password) VALUES('{EmailGenerator()}','{PasswordGenerator()}')";
+                cmd.ExecuteNonQuery();
+            }
+            for (int i = 0; i < amountOfBooks; i++)
+            {
+                cmd.CommandText = $"INSERT INTO books(author, name, pages) VALUES('{AuthorGenerator()}','Some name {i}', {PagesGenerator()})";
+                cmd.ExecuteNonQuery();
+            }
+            for (int i = 0; i < amountOfStatistic; i++)
+            {
+                cmd.CommandText = $"INSERT INTO statistic(user_id, book_id, readed_pages, review) VALUES({random.Next(1, amountOfUsers)},{random.Next(1, amountOfBooks)}, 0, '{reviewTemplate}')";
+                cmd.ExecuteNonQuery();
+            }
+
+        }
         static void Main(string[] args)
         {
             var connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=read-books-db";
 
             using var connection = new NpgsqlConnection(connectionString);
 
-            connection.Open();
+            InsertData(connectionString, connection);
+
+            //connection.Open();
 
             //var sql = "SELECT * FROM users";
 
