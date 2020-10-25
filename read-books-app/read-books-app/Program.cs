23f9a9b73c6cc81e4b5e8358699412150a6c0d2b
Erika Sypko <sypko.erika.2000@gmail.com>
2020-10-25 19:14:04 +0200

    Fixed table view

--- read-books-app/read-books-app/Program.cs
+++ read-books-app/read-books-app/Program.cs
@@ -59,55 +59,76 @@ namespace read_books_app
             }
 
         }
-        static void Main(string[] args)
+        static void PrintUsers(NpgsqlConnection connection)
         {
-            var connectionString = "Host=localhost;Username=postgres;Password=postgres;Database=read-books-db";
-
-            using var connection = new NpgsqlConnection(connectionString);
-
-            InsertData(connectionString, connection);
-
+            Console.WriteLine("\n\t\t\tUsers Table\n");
             connection.Open();
 
             var sql = "SELECT * FROM users";
-            
 
             using var cmd = new NpgsqlCommand(sql, connection);
-           
 
             using NpgsqlDataReader rdr = cmd.ExecuteReader();
-            Console.WriteLine($"{rdr.GetName(0),-4}\t {rdr.GetName(1),-10}\t {rdr.GetName(2),10}\t");
+
+            Console.WriteLine("{0,-4}    |    {1,-25}    |    {2,-25}\n", rdr.GetName(0), rdr.GetName(1),
+                rdr.GetName(2));
 
             while (rdr.Read())
             {
-                Console.WriteLine($"{rdr.GetInt32(0),-4}\t {rdr.GetString(1),-10}\t {rdr.GetString(2),-10}\t");
+                Console.WriteLine("{0,-4}    |    {1,-25}    |    {2,-25}", rdr.GetInt32(0), rdr.GetString(1),
+                    rdr.GetString(2));
             }
             connection.Close();
+        }
+
+        static void PrintBooks(NpgsqlConnection connection)
+        {
+            Console.WriteLine("\n\t\t\t\tBooks Table\n");
             connection.Open();
             var sqlb = "SELECT * FROM books";
             using var cmdb = new NpgsqlCommand(sqlb, connection);
             using NpgsqlDataReader rdrb = cmdb.ExecuteReader();
-            
-            Console.WriteLine("\tBooks\t\n");
-            Console.WriteLine($"{rdrb.GetName(0),-4}\t {rdrb.GetName(1),-10}\t {rdrb.GetName(2),10}\t {rdrb.GetName(3),-4}\t");
+
+            Console.WriteLine("{0,-5}    |    {1,-18}    |    {2,-12}    |    {3,-10}\n", rdrb.GetName(0), rdrb.GetName(1),
+                rdrb.GetName(2), rdrb.GetName(3));
 
             while (rdrb.Read())
             {
-                Console.WriteLine($"{rdrb.GetInt32(0),-4}\t {rdrb.GetString(1),-10}\t {rdrb.GetString(2),-10}\t {rdrb.GetInt32(3),-4}\t");
+                Console.WriteLine("{0,-5}    |    {1,-18}    |    {2,-12}    |    {3,-10}", rdrb.GetInt32(0), rdrb.GetString(1),
+                    rdrb.GetString(2), rdrb.GetInt32(3));
             }
             connection.Close();
+        }
+
+        static void PrintStatistic(NpgsqlConnection connection)
+        {
+            Console.WriteLine("\n\t\t\t\t\tStatistic Table\n");
             connection.Open();
             var sqls = "SELECT * FROM statistic";
             using var cmds = new NpgsqlCommand(sqls, connection);
             using NpgsqlDataReader rdrs = cmds.ExecuteReader();
 
-            Console.WriteLine("\tStaticsic\t\n");
-            Console.WriteLine($"{rdrs.GetName(0),-4}\t {rdrs.GetName(1),-4}\t {rdrs.GetName(2),-4}\t {rdrs.GetName(3),-4}\t {rdrb.GetName(4),10}\t");
+            Console.WriteLine("{0,-8}    |    {1,-8}    |    {2,-8}    |    {3,-15}    |    {4,-6}\n", rdrs.GetName(0), rdrs.GetName(1),
+                rdrs.GetName(2), rdrs.GetName(3), rdrs.GetName(4));
 
             while (rdrs.Read())
             {
-                Console.WriteLine($"{rdrs.GetInt32(0),-4}\t {rdrs.GetInt32(1),-4}\t {rdrs.GetInt32(2),-4}\t {rdrs.GetInt32(3),-4}\t {rdrb.GetString(4),-10}\t");
+                Console.WriteLine("{0,-8}    |    {1,-8}    |    {2,-8}    |    {3,-15}    |    {4,-6}...", rdrs.GetInt32(0), rdrs.GetInt32(1),
+                    rdrs.GetInt32(2), rdrs.GetInt32(3), rdrs.GetString(4).Substring(0, rdrs.GetString(4).Length / 20));
             }
+            connection.Close();
+        }
+        static void Main(string[] args)
+        {
+            var connectionString = "Host=localhost;Username=postgres;Password=joanne19!;Database=read-books-db";
+
+            using var connection = new NpgsqlConnection(connectionString);
+
+            //InsertData(connectionString, connection);
+
+            PrintUsers(connection);
+            PrintBooks(connection);
+            PrintStatistic(connection);
         }
     }
 }
