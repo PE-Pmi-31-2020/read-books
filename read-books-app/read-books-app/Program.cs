e52aed64f2efd0931c0b2587bdeabdf515d3f435
Victoriabuhai <tori001.b@gmail.com>
2020-10-25 17:58:07 +0200

    Print data base

--- read-books-app/read-books-app/Program.cs
+++ read-books-app/read-books-app/Program.cs
@@ -67,19 +67,47 @@ namespace read_books_app
 
             InsertData(connectionString, connection);
 
-            //connection.Open();
+            connection.Open();
 
-            //var sql = "SELECT * FROM users";
+            var sql = "SELECT * FROM users";
+            
 
-            //using var cmd = new NpgsqlCommand(sql, connection);
+            using var cmd = new NpgsqlCommand(sql, connection);
+           
 
-            //using NpgsqlDataReader rdr = cmd.ExecuteReader();
+            using NpgsqlDataReader rdr = cmd.ExecuteReader();
+            Console.WriteLine($"{rdr.GetName(0),-4}\t {rdr.GetName(1),-10}\t {rdr.GetName(2),10}\t");
 
-            //while (rdr.Read())
-            //{
-            //    Console.WriteLine("{0} {1} {2}", rdr.GetInt32(0), rdr.GetString(1),
-            //            rdr.GetString(2));
-            //}
+            while (rdr.Read())
+            {
+                Console.WriteLine($"{rdr.GetInt32(0),-4}\t {rdr.GetString(1),-10}\t {rdr.GetString(2),-10}\t");
+            }
+            connection.Close();
+            connection.Open();
+            var sqlb = "SELECT * FROM books";
+            using var cmdb = new NpgsqlCommand(sqlb, connection);
+            using NpgsqlDataReader rdrb = cmdb.ExecuteReader();
+            
+            Console.WriteLine("\tBooks\t\n");
+            Console.WriteLine($"{rdrb.GetName(0),-4}\t {rdrb.GetName(1),-10}\t {rdrb.GetName(2),10}\t {rdrb.GetName(3),-4}\t");
+
+            while (rdrb.Read())
+            {
+                Console.WriteLine($"{rdrb.GetInt32(0),-4}\t {rdrb.GetString(1),-10}\t {rdrb.GetString(2),-10}\t {rdrb.GetInt32(3),-4}\t");
+            }
+            connection.Close();
+            connection.Open();
+            var sqls = "SELECT * FROM statistic";
+            using var cmds = new NpgsqlCommand(sqls, connection);
+            using NpgsqlDataReader rdrs = cmds.ExecuteReader();
+
+            Console.WriteLine("\tStaticsic\t\n");
+            Console.WriteLine($"{rdrs.GetName(0),-4}\t {rdrs.GetName(1),-4}\t {rdrs.GetName(2),-4}\t {rdrs.GetName(3),-4}\t {rdrb.GetName(4),10}\t");
+
+            while (rdrs.Read())
+            {
+                Console.WriteLine($"{rdrs.GetInt32(0),-4}\t {rdrs.GetInt32(1),-4}\t {rdrs.GetInt32(2),-4}\t {rdrs.GetInt32(3),-4}\t {rdrb.GetString(4),-10}\t");
+            }
         }
     }
 }
