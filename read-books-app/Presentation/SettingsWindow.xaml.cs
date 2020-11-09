a969acdab33a18ea4ae9969e7d482c6072a5a53a
Victoriabuhai <tori001.b@gmail.com>
2020-11-09 17:47:59 +0200

    fixed clock in setting window

--- read-books-app/Presentation/SettingsWindow.xaml.cs
+++ read-books-app/Presentation/SettingsWindow.xaml.cs
@@ -21,5 +21,68 @@ namespace Presentation
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int time;
+            int time_m;
+
+            if (Convert.ToInt32(MinuteTextBox.Text) > 58 && Convert.ToInt32(HourTextBox.Text) > 22)
+            {
+                time = 0;
+                HourTextBox.Text = Convert.ToString(time);
+                MinuteTextBox.Text = Convert.ToString(time);
+
+
+            }
+            else if (Convert.ToInt32(MinuteTextBox.Text) > 58)
+            {
+                time_m = 0;
+                time = Convert.ToInt32(HourTextBox.Text) + 1;
+                HourTextBox.Text = Convert.ToString(time);
+                MinuteTextBox.Text = Convert.ToString(time_m);
+
+            }
+            else
+            {
+                time = Convert.ToInt32(MinuteTextBox.Text) + 1;
+                MinuteTextBox.Text = Convert.ToString(time);
+            }
+
+        }
+
+        private void SubClockButton_Click(object sender, RoutedEventArgs e)
+        {
+            int time;
+            if (Convert.ToInt32(MinuteTextBox.Text) < 1 && Convert.ToInt32(HourTextBox.Text) < 1)
+            {
+                HourTextBox.Text = Convert.ToString(23);
+                MinuteTextBox.Text = Convert.ToString(59);
+
+            }
+            else if (Convert.ToInt32(MinuteTextBox.Text) < 2)
+            {
+                time = Convert.ToInt32(HourTextBox.Text) - 1;
+                HourTextBox.Text = Convert.ToString(time);
+                MinuteTextBox.Text = Convert.ToString(59);
+
+            }
+            else
+            {
+                time = Convert.ToInt32(MinuteTextBox.Text) - 1;
+                MinuteTextBox.Text = Convert.ToString(time);
+            }
+        }
+
+        private void MinuteTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !(Char.IsDigit(e.Text, 0));
+        }
+
+        private void HourTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !(Char.IsDigit(e.Text, 0));
+        }
     }
+
 }
