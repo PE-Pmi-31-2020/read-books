f649cfd3b6f7a4cc1004c95eda944117cc40620a
Erik <erik.mirai@outlook.com>
2020-11-21 19:50:43 +0200

    UnitOfWork added

---
+++ read-books-app/DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Interfaces
+{
+    interface IUnitOfWork : IDisposable
+    {
+        IRepository<Book> Books { get; }
+        IRepository<Statistic> Statistics { get; }
+        IRepository<User> Users { get; }
+        void Save();
+    }
+}
---
+++ read-books-app/DAL/Repositories/EFUnitOfWork.cs
@@ -0,0 +1,76 @@
+﻿using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repositories
+{
+    class EFUnitOfWork : IUnitOfWork
+    {
+        private ReadBooksContext db;
+        private BookRepository bookRepository;
+        private StatisticRepository statisticRepository;
+        private UserRepository userRepository;
+        private bool disposed = false;
+        //public EFUnitOfWork(string connectionString)
+        //{
+        //    db = new ReadBooksContext(connectionString);
+        //}
+        public IRepository<Book> Books
+        {
+            get
+            {
+                if(bookRepository == null)
+                {
+                    bookRepository = new BookRepository(db);
+                }
+                return bookRepository;
+            }
+        }
+
+        public IRepository<Statistic> Statistics
+        {
+            get
+            {
+                if (statisticRepository == null)
+                {
+                    statisticRepository = new StatisticRepository(db);
+                }
+                return statisticRepository;
+            }
+        }
+
+        public IRepository<User> Users
+        {
+            get
+            {
+                if (userRepository == null)
+                {
+                    userRepository = new UserRepository(db);
+                }
+                return userRepository;
+            }
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
